29c2c533183899427e295befa148bd2d7285555f
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2023-03-26 15:31:36 +0200

    wip: fix prefix

--- next.config.js
+++ next.config.js
@@ -6,11 +6,10 @@ const isProd = process.env.NODE_ENV === 'production'
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  assetPrefix: isProd ? '/aaroninvent/' : '',
+  assetPrefix: isProd ? 'aaroninvent' : '',
   images: {
     unoptimized: true,
   },
-  basePath: '/ghtest',
 }
 
 module.exports = nextConfig
